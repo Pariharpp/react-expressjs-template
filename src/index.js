6056112f91d27c02560b9b0ce20dbb9e167f4c28
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-10-07 18:43:38 +0530

    popper jquery bootstrap axios

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+// JS
+window.Popper = require('popper.js').default;
+window.$ = window.jQuery = require('jquery');
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+require('bootstrap');
+// CSS
 import styles from './scss/app.scss';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
